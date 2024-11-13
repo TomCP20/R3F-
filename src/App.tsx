2ee177e948a22fab16c831e156b153e8a911d28b
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-11-13 19:31:35 +0000

    setup raymarching

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
-import { OrbitControls } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+import { Canvas, useFrame, useThree } from "@react-three/fiber"
+import { useRef } from "react";
+import { ShaderMaterial, Uniform, Vector2 } from "three";
+
+import fragmentShader from './shaders/fragmentShader.glsl??raw'
+import vertexShader from './shaders/vertexShader.glsl??raw'
+
+const DPR = 1;
 
 export default function App() {
   return (
     <div className="size-[800px] text-center m-auto">
-      <Canvas className="bg-black" gl={{ preserveDrawingBuffer: true }}>
-        <mesh>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshPhongMaterial />
-        </mesh>
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} color="red" />
-        <OrbitControls />
+      <Canvas camera={{ position: [0, 0, 6] }} dpr={DPR} className="bg-black" gl={{ preserveDrawingBuffer: true }}>
+        <Sdf />
       </Canvas>
     </div>
   )
+}
+
+function Sdf() {
+  const shaderRef = useRef<ShaderMaterial>(null!);
+  const {viewport} = useThree();
+
+  const uniforms = {
+    uTime: new Uniform(0.0),
+    uResolution: new Uniform(new Vector2()),
+  }
+
+  useFrame((state) => {
+    const { clock } = state;
+    shaderRef.current.uniforms.uTime.value = clock.getElapsedTime();
+    shaderRef.current.uniforms.uResolution.value = new Vector2(
+      800 * DPR,
+      800 * DPR
+    );
+  });
+
+  return (
+    <mesh scale={[viewport.width, viewport.height, 1]}>
+      <planeGeometry args={[1, 1]} />
+      <shaderMaterial
+        ref={shaderRef}
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        uniforms={uniforms}
+      />
+    </mesh>
+
+  );
 }
---
+++ src/shaders/fragmentShader.glsl
@@ -0,0 +1,96 @@
+uniform float uTime;
+uniform vec2 uResolution;
+
+#define MAX_STEPS 100
+#define MAX_DIST 100.0
+#define SURFACE_DIST 0.01
+
+float sdSphere(vec3 p, float radius)
+{
+    return length(p) - radius;
+}
+
+float scene(vec3 p)
+{
+    float plane = p.y + 1.0;
+    float sphere1 = sdSphere(p - vec3(1.0 + cos(uTime), 0.7, 0.0), 1.0);
+    float sphere2 = sdSphere(p + vec3(1.0, 0.5 + sin(uTime) / 2.0, 0.0), 1.0);
+
+    float distance1 = min(sphere1, sphere2);
+    float distance2 = min(plane, distance1);
+    return distance2;
+}
+
+float raymarch(vec3 ro, vec3 rd)
+{
+    float dO = 0.0;
+    vec3 color = vec3(0.0);
+
+    for(int i = 0; i < MAX_STEPS; i++)
+    {
+        vec3 p = ro + rd * dO;
+        float dS = scene(p);
+
+        dO += dS;
+
+        if(dO > MAX_DIST || dS < SURFACE_DIST)
+        {
+            break;
+        }
+    }
+    return dO;
+}
+
+vec3 getNormal(vec3 p)
+{
+    vec2 e = vec2(.01, 0);
+
+    vec3 n = scene(p) - vec3(scene(p - e.xyy), scene(p - e.yxy), scene(p - e.yyx));
+
+    return normalize(n);
+}
+
+float softShadows(vec3 ro, vec3 rd, float mint, float maxt, float k)
+{
+    float resultingShadowColor = 1.0;
+    float t = mint;
+    for(int i = 0; i < 50 && t < maxt; i++)
+    {
+        float h = scene(ro + rd * t);
+        if(h < 0.001)
+            return 0.0;
+        resultingShadowColor = min(resultingShadowColor, k * h / t);
+        t += h;
+    }
+    return resultingShadowColor;
+}
+
+void main()
+{
+    vec2 uv = gl_FragCoord.xy / uResolution.xy;
+    uv -= 0.5;
+    uv.x *= uResolution.x / uResolution.y;
+
+    // Light Position
+    vec3 lightPosition = vec3(-10.0, 10.0, 10.0);
+
+    vec3 ro = vec3(0.0, 0.0, 5.0);
+    vec3 rd = normalize(vec3(uv, -1.0));
+
+    float d = raymarch(ro, rd);
+    vec3 p = ro + rd * d;
+
+    vec3 color = vec3(0.0);
+
+    if(d < MAX_DIST)
+    {
+        vec3 normal = getNormal(p);
+        vec3 lightDirection = normalize(lightPosition - p);
+
+        float diffuse = max(dot(normal, lightDirection), 0.0);
+        float shadows = softShadows(p, lightDirection, 0.1, 5.0, 64.0);
+        color = vec3(1.0, 1.0, 1.0) * diffuse * shadows;
+    }
+
+    gl_FragColor = vec4(color, 1.0);
+}
