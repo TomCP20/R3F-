5f03a7836ae0772ea0d7a15d5e4460e203a2b43b
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-11-27 20:39:50 +0000

    refactoring

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -2,7 +2,7 @@ uniform float uTime;
 uniform vec2 uResolution;
 
 #define MAX_STEPS 100
-#define MAX_DIST 100.0
+#define MAX_DIST 200.0
 #define SURFACE_DIST 0.01
 #define M_PI 3.1415926535897932384626433832795
 
@@ -122,7 +122,7 @@ float softShadows(vec3 ro, vec3 rd, float mint, float maxt, float k)
     return resultingShadowColor;
 }
 
-vec3 getLookAtDir(vec2 uv, vec3 rayOrigin, vec3 lookAt) 
+vec3 getLookAtDir(vec2 uv, vec3 rayOrigin, vec3 lookAt)
 {
     vec3 f = normalize(lookAt - rayOrigin);
     vec3 r = cross(vec3(0.0, 1.0, 0.0), f);
@@ -138,39 +138,20 @@ void main()
     uv -= 0.5;
     uv.x *= uResolution.x / uResolution.y;
 
-    // Light Position
-    vec3 lightPosition = rotateY3D(-M_PI / 5.0) * vec3(10.0, 10.0, 10.0);
+    vec3 rayOrigin = vec3(-10.0 * uTime, 10.0 * sin(uTime), 0.0);
 
-    vec3 rayOrigin = vec3(0.0, 1.5, 4.0);
-
-    vec3 rayDirection = getLookAtDir(uv, rayOrigin, vec3(0.0));
-    //vec3 rayDirection = normalize(vec3(uv, -1.0));
+    //vec3 rayDirection = getLookAtDir(uv, rayOrigin, vec3(0.0));
+    vec3 rayDirection = rotateY3D(uTime) * normalize(vec3(uv, -1.0));
 
     float distance = raymarch(rayOrigin, rayDirection);
     vec3 p = rayOrigin + rayDirection * distance;
 
     vec3 color = vec3(0.0);
 
-    int material_type = 0;
     if(distance < MAX_DIST)
     {
-        if(material_type == 0)
-        {
-            vec3 normal = getNormal(p);
-            color = normal * 0.5 + 0.5;
-        }
-        else if(material_type == 1)
-        {
-            vec3 normal = getNormal(p);
-            vec3 lightDirection = normalize(lightPosition - p);
-            float diffuse = max(dot(normal, lightDirection), 0.0);
-            float shadows = softShadows(p, lightDirection, 0.1, 5.0, 64.0);
-            color = vec3(1.0) * diffuse * shadows;
-        }
-        else
-        {
-            color = vec3(1.0);
-        }
+        vec3 normal = getNormal(p);
+        color = normal * 0.5 + 0.5;
     }
     gl_FragColor = vec4(color, 1.0);
 }
